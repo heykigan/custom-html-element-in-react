5b662781210286672873353cc666feb69572e18b
Kigan Khadka <kiganbot@gmail.com>
2019-07-12 12:42:47 +0545

    html element wrapper in react app

--- src/index.js
+++ src/index.js
@@ -2,9 +2,29 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import '@webcomponents/custom-elements';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class searchMask extends HTMLElement {
+  constructor() {
+    super(); // always call super() first in the constructor.
+
+  }
+
+  connectedCallback() {
+  	let mountPoint = document.createElement('span');
+    this.appendChild(mountPoint);
+  	ReactDOM.render(<App />,mountPoint);
+  }
+  disconnectedCallback() {
+
+  }
+  attributeChangedCallback(attrName, oldVal, newVal) {
+
+  }
+}
+
+customElements.define('search-mask', searchMask);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
